ae4572c5c4c71dccd52a74e8acfed1a903234279
LeoAdamek <leo@adamek.me>
2013-08-10 00:46:36 +0100

    Added funcions.el

---
+++ functions.el
@@ -0,0 +1,34 @@
+;;
+;;
+;; Leo Adamek's emacs.d
+;;
+;;
+;; functions.el
+;;
+;; Useful universal functions, ideal at any time.
+;;
+;; License: GPLv2
+;;
+
+
+;;==========================================================
+;;  Buffer Functions;
+;;==========================================================
+
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
+(defun kill-all-dired-buffer ()
+  "Kill all dired buffers."
+  (interactive)
+  (save-excursion
+    (let ((count 0))
+      (dolist (buffer (buffer-list))
+	(set-buffer buffer)
+	(when (equal major-mode 'dired-mode)
+	  (setq count (1+ count))
+	  (kill-buffer buffer)))
+      (message "Killed %i dired buffer(s)." count))))
+
