09e180d2cb8230e938230360bf78abde9a3e169d
Leo Adamek <leo@adamek.me>
2013-12-20 11:58:59 +0000

    Marked moe-dark as a safe theme

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4e1a057fe9a9981e886bd0673d131da904e3306c884715b8bee333ef95303e39" "1177fe4645eb8db34ee151ce45518e47cc4595c3e72c55dc07df03ab353ad132" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "847c431bd5eb5017b601f4c599fb4b23b096e48739e4a708f57ac756358751f0" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "9873d7793e0449ed30b78358a81d8219622aabf9df4492c22e08b247751ade5c" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "246a51f19b632c27d7071877ea99805d4f8131b0ff7acb8a607d4fd1c101e163" default)))
+ '(custom-safe-themes (quote ("6a8f4e2916afba0c699fb8672aa811b190bdeedec2f196668301e8254ed3a262" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4e1a057fe9a9981e886bd0673d131da904e3306c884715b8bee333ef95303e39" "1177fe4645eb8db34ee151ce45518e47cc4595c3e72c55dc07df03ab353ad132" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "847c431bd5eb5017b601f4c599fb4b23b096e48739e4a708f57ac756358751f0" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "9873d7793e0449ed30b78358a81d8219622aabf9df4492c22e08b247751ade5c" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "246a51f19b632c27d7071877ea99805d4f8131b0ff7acb8a607d4fd1c101e163" default)))
  '(haskell-font-lock-haddock t)
  '(haskell-font-lock-symbols t)
  '(haskell-hoogle-command "")
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 40 :foundry "xos4" :family "Terminus")))))
+ '(default ((t (:height 80 :foundry "xos4" :family "Terminus")))))
