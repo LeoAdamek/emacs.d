3f0cce523e32b4a3a54422cbda5eb54a4b64c375
Leo Adamek <leo@adamek.me>
2014-01-23 16:00:02 +0000

    Changed options, added fringe, lots of cool stuff in here :+1:

--- options.el
+++ options.el
@@ -6,18 +6,66 @@
 ;; This file is licensed under GPLv2
 ;;
 
+;; The Font
+(set-default-font "Terminus 8")
+
 ;; Turn off GUI elements
 ;; Pretty self explainitory.
-(menu-bar-mode 1)
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 ;; Disable the default splash screen
 (setq inhibit-splash-screen t)
 
-;; Turn ON ido-mode
-;; NOTE: I've disabled this to try and use HELM
-;;(ido-mode 1)
+;; Set the *scratch* buffer default to ""
+(setq initial-scratch-message "")
+
+;; Minimal Cursor
+(setq cursor-type 'hbar)
+
+;; Add a fringe.
+;; It's quite nice.
+(set-fringe-mode
+ (/ (- (frame-pixel-width)
+       (* 200 (frame-char-width)))
+    4))
+
+;; Set some colours
+;; Black background, black fringe, grey text
+(custom-set-faces
+ '(default ((t (:background "black" :foreground "grey") )) )
+ '(fringe  ((t (:background "black") )) ) )
+
+;; Turn off the mode line Ö :shocking:
+(defvar-local hidden-mode-line-mode nil)
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global nil
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line-mode mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden mode line enabled. "
+             "Use M-x hidden-mode-line-mode RET to return it.") )))
+
+;; Have a mode-line in C-S-SPC in the header
+;; ATTENTION: You do need to deactivate hidden-mode-line-mode.
+(defun mode-line-in-header ()
+  (interactive)
+  (if (not header-line-format)
+      (setq header-line-format mode-line-format)
+    (setq header-line-format nil))
+  (force-mode-line-update))
+ (global-set-key (kbd "C-s") 'mode-line-in-header)
 
 ;; Put all backups in the same place.
 ;; Avoid polluting repositories.
