30be75a273639ee662f429d9265116795b6a7cd2
Leo Adamek <leo@adamek.me>
2014-11-23 21:40:53 +0000

    Added moe-dark as a safe theme.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("09233dff5af535c4ba3ccabc4c9267bb7bf1131cccbfab5db65e96103c7aa023" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "454dc6f3a1e9e062f34c0f988bcef5d898146edc5df4aa666bf5c30bed2ada2e" default)))
+    ("8d33dd4dc3a1c6c360f5391a949ceba1c5762a035044b29381dae04eaf61e77e" "09233dff5af535c4ba3ccabc4c9267bb7bf1131cccbfab5db65e96103c7aa023" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "454dc6f3a1e9e062f34c0f988bcef5d898146edc5df4aa666bf5c30bed2ada2e" default)))
  '(safe-local-variable-values
    (quote
     ((file-header-author . "Leo Adamek <leo.adamek@mrzen.com>")
@@ -16,5 +16,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#101010" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "CodingFontTobi"))))
- '(fringe ((t (:background "#101010")))))
+ )
