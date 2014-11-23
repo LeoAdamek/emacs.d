83c8e3a3a49bc9c4b131b626683bfc9ca6c8189e
Leo Adamek <leo@adamek.me>
2014-11-23 13:08:15 +0000

    Added 'origami' to default packages

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,19 +1,22 @@
 (require 'package)
 (defvar package-list
+  ;; These packages will be required and installed automatically.
+  ;; If you've forked this, go ahead and edit this as you want
   '(
     php-mode
     nxml
     helm
     yasnippet
-;;    moe-theme
     helm-gist
     auto-complete
     rainbow-delimiters
     magit
+    origami
     )
 )
 
-;; Add Package Archives
+;; Add Package Archives (MELPA and Marmalade)
+;; Yes, I know MELPA is unpopular but whatever.
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
@@ -25,12 +28,14 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+;; Install all the requested packages
 (dolist (package package-list)
   (when (not (package-installed-p package))
     (package-install package)))
 
 
 ;; Load SLIME if SBCL and SLIME are installed.
+;; Slime is /not/ installed via package.el
 (when (file-exists-p "~/.emacs.d/packages/slime/slime.el")
       (add-to-list 'load-path "~/.emacs.d/packages/slime")
       (require 'slime-autoloads)
