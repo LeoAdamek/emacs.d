4d9c73fcc3b4ec73328f5df1460a167051cec23c
Leo Adamek <leo.adamek@mrzen.com>
2015-01-12 17:19:01 +0000

    Tweaking settings for auto-complete

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu t)
+ '(ac-auto-start t)
+ '(ac-candidate-limit 10)
  '(custom-safe-themes
    (quote
     ("cfe99939ab2423c01d34c97f6ae40e1db965564a7acb17731eb049a2e5e76392" "8d33dd4dc3a1c6c360f5391a949ceba1c5762a035044b29381dae04eaf61e77e" "09233dff5af535c4ba3ccabc4c9267bb7bf1131cccbfab5db65e96103c7aa023" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "454dc6f3a1e9e062f34c0f988bcef5d898146edc5df4aa666bf5c30bed2ada2e" default)))
@@ -16,4 +19,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background "#111" :family "CodingFontTobi" :height 120))))
+ '(ac-candidate-face ((t (:inherit apropos-property))))
+ '(fringe ((t (:background "#111")))))
