e7cf834898ac3e5b1be784b0df3f723a969c1240
Leo Adamek <leo@adamek.me>
2015-01-23 23:00:15 +0000

    Updated custom.el

--- custom.el
+++ custom.el
@@ -16,4 +16,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#111" :foreground "#c6c6c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "altsys" :family "Dejavu Sans Mono"))))
+ '(fringe ((t (:background "#111")))))
