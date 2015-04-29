af6ecb19ce6bf9f1152bc0cb113c967b4499e1c5
Leo Adamek <leo@adamek.me>
2015-04-29 22:24:16 +0100

    Updated custom.el

--- custom.el
+++ custom.el
@@ -9,4 +9,11 @@
  '(custom-safe-themes
    (quote
     ("cfe99939ab2423c01d34c97f6ae40e1db965564a7acb17731eb049a2e5e76392" "8d33dd4dc3a1c6c360f5391a949ceba1c5762a035044b29381dae04eaf61e77e" "09233dff5af535c4ba3ccabc4c9267bb7bf1131cccbfab5db65e96103c7aa023" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "454dc6f3a1e9e062f34c0f988bcef5d898146edc5df4aa666bf5c30bed2ada2e" default)))
- )
+ '(haskell-font-lock-symbols t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#111" :height 120))))
+ '(fringe ((t (:background "#111")))))
