4bb79181d8305212421578ec41dfb69095b7fd6c
Leo Adamek <leo@adamek.me>
2017-08-14 22:22:49 +0100

    Removing `hidden-mode-line-mode' as I never used it.

--- lisp/options.el
+++ lisp/options.el
@@ -44,47 +44,6 @@
 (add-hook 'after-make-frame-functions '*make-frame-pretty t)
 
 
-;; Turn off the mode line Ö :shocking:
-(defvar-local hidden-mode-line-mode nil)
-(define-minor-mode hidden-mode-line-mode
-  "Minor mode to hide the mode-line in the current buffer."
-  :init-value nil
-  :global t
-  :variable hidden-mode-line-mode
-  :group 'editing-basics
-  (if hidden-mode-line-mode
-      (setq hide-mode-line mode-line-format
-            mode-line-format nil)
-    (setq mode-line-format hide-mode-line
-          hide-mode-line nil))
-  (when (and (called-interactively-p 'interactive)
-             hidden-mode-line-mode)
-    (run-with-idle-timer
-     0 nil 'message
-     (concat "Hidden mode line enabled. "
-             "Use M-x hidden-mode-line-mode RET to return it.") )))
-
-;; Option to turn it on *everywhere*
-(define-globalized-minor-mode global-hidden-mode-line-mode hidden-mode-line-mode hidden-mode-line-mode-on)
-
-(defun hidden-mode-line-mode-on ()
-  (hidden-mode-line-mode 1))
-
-
-;; And turn it on!
-;; (hidden-mode-line-mode t)
-
-(semantic-mode)
-
-;; Have a mode-line in C-s-c in the header
-(defun mode-line-in-header ()
-  (interactive)
-  (if (not header-line-format)
-      (setq header-line-format mode-line-format)
-    (setq header-line-format nil))
-  (force-mode-line-update))
- (global-set-key (kbd "C-s-c") 'mode-line-in-header)
-
 ;; Put all backups in the same place.
 ;; Avoid polluting repositories.
 ;; The fact that this isn't default behaviour is sinful.
