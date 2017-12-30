58499e21b2997061bc6f3078e8a606d6bf00cc9c
Leo Adamek <leo@adamek.me>
2017-12-30 15:37:12 +0000

    Updated packages.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-restclient restclient-helm restclient typescript tide rvm ruby-interpolation ruby-hash-syntax ruby-guard ruby-end ruby-block rubocop rspec-mode rsense robe rinari rainbow-delimiters php-mode php-completion origami magit logstash-conf helm-robe helm-projectile helm-flyspell helm-company haskell-mode haml-mode flymake-haml flx-ido elixir-mode company-go company-emoji company-auctex clojure-mode auto-complete))))
+    (racer company-racer rust-playground rust-mode yaml-mode tide tablist robe restclient-helm rainbow-delimiters php-mode origami nim-mode markdown-mode magit keyfreq jq-mode jekyll-modes helm-robe helm-projectile hackernews hacker-typer golint god-mode gobgen go-stacktracer go-snippets go-playground go-add-tags flymake-elixir flx-ido elixir-yasnippets elixir-mode elixir-mix csv-mode confluence company-terraform company-statistics company-restclient company-go company-c-headers company-auctex coin-ticker cobol-mode cljr-helm clang-format browse-at-remote brainfuck-mode auto-complete-auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
